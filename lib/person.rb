91b95f440eb494aaec479cd1c421d60dcbdf274d
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-21 03:20:05 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
+Beyonce = Person.new
+Beyonce.name
+
+def initialize(job)
+  @job = job
+end
+
+def job
+  @job
+end
+
+beyonce = Person.new
+job = "Singer"
+beyonce.job
